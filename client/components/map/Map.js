6d2b78beaf78a1aa0e643b36eeb3ec6674be30ce
jeru <mikurej95@gmail.com>
2018-10-01 00:27:50 -0500

    refactored map component

--- client/components/map/Map.js
+++ client/components/map/Map.js
@@ -2,32 +2,30 @@ import React from 'react'
 import {connect} from 'react-redux'
 import {withGoogleMap, GoogleMap, withScriptjs} from 'react-google-maps'
 
-class GMap extends React.Component {
-  render() {
-    const {lat, lng} = this.props.coordinates
-    return (
-      <GoogleMap
-        center={new google.maps.LatLng(lat, lng)}
-        defaultZoom={13}
-        defaultOptions={{
-          mapTypeControl: false,
-          fullscreenControl: false,
-          styles: [
-            {
-              featureType: 'poi',
-              stylers: [{visibility: 'off'}]
-            },
-            {
-              featureType: 'transit',
-              elementType: 'labels.icon',
-              stylers: [{visibility: 'off'}]
-            }
-          ]
-          // clickableIcons: false
-        }}
-      />
-    )
-  }
+const GMap = props => {
+  const {lat, lng} = props.coordinates
+  return (
+    <GoogleMap
+      center={new google.maps.LatLng(lat, lng)}
+      defaultZoom={13}
+      defaultOptions={{
+        mapTypeControl: false,
+        fullscreenControl: false,
+        styles: [
+          {
+            featureType: 'poi',
+            stylers: [{visibility: 'off'}]
+          },
+          {
+            featureType: 'transit',
+            elementType: 'labels.icon',
+            stylers: [{visibility: 'off'}]
+          }
+        ]
+        // clickableIcons: false
+      }}
+    />
+  )
 }
 
 const mapState = state => {
