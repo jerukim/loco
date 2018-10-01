fa5487a02a6d2b347b317a2cbc6e624ccc8b3b5a
jeru <mikurej95@gmail.com>
2018-10-01 15:10:12 -0500

    cleaned up styling

--- client/app.js
+++ client/app.js
@@ -6,15 +6,20 @@ import {
   ScreensSidePanel
 } from './screens'
 import Routes from './routes'
+import Grid from '@material-ui/core/Grid'
 
 const App = () => {
   return (
-    <div>
-      <ScreensNavbar />
+    <div id="main">
+      {/* <ScreensNavbar /> */}
+      <div id="top">
+        <ScreensSidePanel />
+        <ScreensMap />
+      </div>
+      <div id="bottom">
+        <ScreensRankingTabs />
+      </div>
       <Routes />
-      <ScreensSidePanel />
-      <ScreensMap />
-      <ScreensRankingTabs />
     </div>
   )
 }
