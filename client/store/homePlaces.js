788651d7297c8c6ed40a7a730446cc6464c56383
jeru <mikurej95@gmail.com>
2018-10-09 13:00:43 -0500

    removed unused actions, dispatchers and thunks

--- client/store/homePlaces.js
+++ client/store/homePlaces.js
@@ -1,8 +1,6 @@
 import axios from 'axios'
 
 const FETCH_ALL_HOME_PLACES_SUCCESS = 'FETCH_ALL_HOME_PLACES_SUCCESS'
-// const FETCH_ONE_HOME_PLACES_SUCCESS = 'FETCH_ONE_HOME_PLACES_SUCCESS'
-// const ADDED_PLACE_FOR_ALL_HOMES = 'ADDED_PLACE_FOR_ALL_HOMES'
 const FETCH_HOME_PLACES_REQUEST = 'FETCH_HOME_PLACES_REQUEST'
 const FETCH_HOME_PLACES_ERROR = 'FETCH_HOME_PLACES_ERROR'
 const DELETED_HOME_IN_HOME_PLACES = 'DELETED_HOME_IN_HOME_PLACES'
@@ -12,10 +10,6 @@ const fetchAllHomePlacesSuccess = homePlaces => ({
   type: FETCH_ALL_HOME_PLACES_SUCCESS,
   homePlaces
 })
-// const fetchOneHomePlacesSuccess = homePlaces => ({
-//   type: FETCH_ONE_HOME_PLACES_SUCCESS,
-//   homePlaces
-// })
 const fetchHomePlacesRequest = () => ({
   type: FETCH_HOME_PLACES_REQUEST
 })
@@ -26,10 +20,6 @@ const deletedHomeInHomePlaces = homeId => ({
   type: DELETED_HOME_IN_HOME_PLACES,
   homeId
 })
-// const addedPlaceForAllHomes = placeId => ({
-//   type: ADDED_PLACE_FOR_ALL_HOMES,
-//   placeId
-// })
 const deletedPlaceInHomePlaces = (placeId, homes) => ({
   type: DELETED_PLACE_IN_HOME_PLACES,
   placeId
@@ -79,16 +69,6 @@ export const fetchOneHomePlaces = (userId, homeId) => async dispatch => {
   }
 }
 
-// get homePlace for each home (adding place)
-// export const addPlaceForAllHomes = placeId => async dispatch => {
-//   try {
-//     dispatch(fetchHomePlacesRequest())
-//     dispatch(addedPlaceForAllHomes(placeId))
-//   } catch (err) {
-//     console.error(err)
-//   }
-// }
-
 // delete place for all homes (remove place)
 export const deletePlaceInHomePlaces = (placeId, homes) => dispatch => {
   try {
@@ -122,10 +102,6 @@ export default function(state = initialState, action) {
         fetchingCategoryResults: false,
         errorFetching: false
       }
-    // case FETCH_ONE_HOME_PLACES_SUCCESS:
-    //   return {...state}
-    // case ADDED_PLACE_FOR_ALL_HOMES:
-    // return
     case DELETED_HOME_IN_HOME_PLACES:
       const removedHomeState = {...state}
       delete removedHomeState[action.homeId]
